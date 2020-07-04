5c1b8f7dc9b0053cef065abc33e04e04df4afed6
LearnerhHub2019 <65852023+learnerhub2019@users.noreply.github.com>
2020-07-04 16:28:55 +0530

    Landing Page Comitt With materal design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import "./styles.css";
+import "./styles.scss";
+//import HeaderComponent from "./components/common/header.component";
+import HomePageComponent from "./components/home/homePage.component";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <HomePageComponent />
     </div>
   );
 }
---
+++ src/components/common/header.component.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HeaderComponent = () => <h2>Header Comes Here</h2>;
+
+export default HeaderComponent;
