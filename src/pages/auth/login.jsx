686a8aa157316b4e31632ae1611a852244189485
LearnerhHub2019 <65852023+learnerhub2019@users.noreply.github.com>
2020-07-05 13:58:22 +0530

    Implement Signin UI

---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginComponent from "../../components/auth/login/login.component";
+// import Container from "@material-ui/core/Container";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginComponent />
+    </div>
+  );
+};
+
+export default LoginPage;
