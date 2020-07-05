759ae5303951d2a16764ce29c2d509eadfc0145c
LearnerhHub2019 <65852023+learnerhub2019@users.noreply.github.com>
2020-07-05 21:57:16 +0530

    Missed Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import Route from "./Route";
 
+//Default pages
 import HomePage from "../pages/home/HomePage";
+import LoginPage from "../pages/auth/login";
+//Private Pages
 import DashboardPage from "../pages/dashboard/Dashboard";
+// Function for outes
 export default function Routes() {
   return (
     <Switch>
+      {/* Default Routes */}
       <Route path="/" exact component={HomePage} />
+      <Route path="/login" exact component={LoginPage} />
+      {/* Private Routes */}
       <Route path="/dashboard" exact component={DashboardPage} isPrivate />
     </Switch>
   );
