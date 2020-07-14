cedc3ad419970df5c0728f39d936ae6ad27882fe
LI310 PC <learnerhub2019@gmail.com>
2020-07-14 19:52:38 +0530

    added branch branch-redux

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,7 @@ export const homeActions = { LOGIN_USER: "LOGIN_USER" };
 export const userActionTypes = {
   FETCH_USERS: "FETCH_USERS"
 };
+
+export const authActionTypes = {
+  LOGIN_USER: "LOGIN_USER"
+};
